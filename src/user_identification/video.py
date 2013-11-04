69b6f8dd22c0021b26def5083df4fff1a1ad994b
David Hoyes <dphoyes@gmail.com>
2013-11-04 00:45:12 +0000

    Added alternative RosVideoSource implementations

--- src/user_identification/video.py
+++ src/user_identification/video.py
@@ -2,10 +2,14 @@
 
 import cv2
 import rospy
-import numpy
+import numpy as np
 from abc import ABCMeta, abstractmethod
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import time
+
+DEFAULT_ROS_VIDEO_TOPIC = "/v4l/camera/image_raw"
+# DEFAULT_ROS_VIDEO_TOPIC = "/rgb/image_raw"
 
 class AbstractVideoSource(object):
     __metaclass__  = ABCMeta
@@ -14,7 +18,6 @@ class AbstractVideoSource(object):
     def getFrame(self):
         """Returns an image frame from the camera, as a 2D numpy array"""
         pass
-
 
 class DirectVideoSource(AbstractVideoSource):
     def __init__(self):
@@ -26,27 +29,49 @@ class DirectVideoSource(AbstractVideoSource):
             raise Exception('Video failed')
         return frame
 
-class RosVideoSource(AbstractVideoSource):
-    rgb_image_path = "/rgb/image_raw"
-    frame = None
-    frame_set = None
+class RosVideoSource1(AbstractVideoSource):
+    ros_video_topic = DEFAULT_ROS_VIDEO_TOPIC
 
     def __init__(self):
-        rospy.wait_for_message(self.rgb_image_path, Image)
+        self.frame = None
         self.cvBridge = CvBridge()
-        self.image_sub = rospy.Subscriber(self.rgb_image_path, Image, self.callback, queue_size=1)
+        rospy.wait_for_message(self.ros_video_topic, Image)
+        self.image_sub = rospy.Subscriber(self.ros_video_topic, Image, self.callback, queue_size=1)
 
-    def callback(self, data):
-        try:
-            frame_mat = self.cvBridge.imgmsg_to_cv(data, "bgr8")
-            self.frame = numpy.asarray(frame_mat)
-            self.frame_set = True
-        except CvBridgeError, e:
-            self.frame_set = False
-            print e
-            raise Exception('RosVideo Failed')
+    def callback(self, ros_frame):
+        self.frame = np.asarray(self.cvBridge.imgmsg_to_cv(ros_frame, "bgr8"))
 
     def getFrame(self):
-        while not self.frame_set:
-            print "Waiting for frame..."
+        while self.frame == None:
+            time.sleep(0.01)
         return self.frame
+
+class RosVideoSource2(AbstractVideoSource):
+    ros_video_topic = DEFAULT_ROS_VIDEO_TOPIC
+
+    def __init__(self):
+        self.ros_frame = None
+        self.cvBridge = CvBridge()
+        rospy.wait_for_message(self.ros_video_topic, Image)
+        self.image_sub = rospy.Subscriber(self.ros_video_topic, Image, self.callback, queue_size=1)
+
+    def callback(self, ros_frame):
+        self.ros_frame = ros_frame
+
+    def getFrame(self):
+        while self.ros_frame == None:
+            time.sleep(0.01)
+        frame = np.asarray(self.cvBridge.imgmsg_to_cv(self.ros_frame, "bgr8"))
+        return frame
+
+class RosVideoSource3(AbstractVideoSource):
+    ros_video_topic = DEFAULT_ROS_VIDEO_TOPIC
+
+    def __init__(self):
+        self.cvBridge = CvBridge()
+
+    def getFrame(self):
+        ros_frame = rospy.wait_for_message(self.ros_video_topic, Image)
+        frame = np.asarray(self.cvBridge.imgmsg_to_cv(ros_frame, "bgr8"))
+        return frame
+
