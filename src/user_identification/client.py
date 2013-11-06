3b8f4fe96f5b9818f5fd549cdd358611e70e5338
David Hoyes <dphoyes@gmail.com>
2013-11-06 22:48:18 +0000

    Changed queryPerson client to return named tuple

--- src/user_identification/client.py
+++ src/user_identification/client.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2
 
 import rospy
+from collections import namedtuple
 from user_identification import srv
 
 PKG_NAME = 'user_identification'
 DEFINE_PERSON_SERVICE_NAME = PKG_NAME+'/definePerson'
 QUERY_PERSON_SERVICE_NAME = PKG_NAME+'/queryPerson'
+
+QueryPersonResult = namedtuple('QueryPersonResult', 'is_person is_known_person id confidence')
 
 def definePerson(person_id):
     rospy.wait_for_service(DEFINE_PERSON_SERVICE_NAME)
@@ -16,5 +19,10 @@ def definePerson(person_id):
 def queryPerson():
     rospy.wait_for_service(QUERY_PERSON_SERVICE_NAME)
     func = rospy.ServiceProxy(QUERY_PERSON_SERVICE_NAME, srv.queryPerson)
-    result = func()
-    return result
+    res = func()
+    return QueryPersonResult(   
+        is_person=res.is_person,
+        is_known_person=res.is_known_person,
+        id=res.id,
+        confidence=res.confidence
+    )
